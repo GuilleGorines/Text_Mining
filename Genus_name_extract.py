8b82e4a485fc3851d5619e1ce32cadb77e703831
GuillermoGorines <60110848+GuillermoGorines@users.noreply.github.com>
2021-01-10 21:14:22 +0100

    Adición de intro a genus_name_extract

--- Genus_name_extract.py
+++ Genus_name_extract.py
@@ -1,3 +1,10 @@
+# Script de extracción de una lista de nombres científicos
+# usando Entrez Utilities.
+# Fecha: 30-12-2020
+# Autor: Guillermo Jorge Gorines Cordero
+# Institución: Instituto de Salud Carlos III
+
+
 
 from Bio import Entrez
 import sys

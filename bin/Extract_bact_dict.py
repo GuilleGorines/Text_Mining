7007b1f497f5712a63c350d28ce84fae30d07339
GuillermoGorines <60110848+GuillermoGorines@users.noreply.github.com>
2021-02-27 20:50:48 +0100

    added error management to extract_bact_dict

--- bin/Extract_bact_dict.py
+++ bin/Extract_bact_dict.py
@@ -4,6 +4,7 @@ import json
 
 dmp_file = sys.argv[1]
 Entrez.email="" # Necesario o error
+error_ids_list = ["IDs que no han podido incluirse en el diccionario:"]
 
 bact_dict = {}
 with open(dmp_file) as dmp:
@@ -21,7 +22,18 @@ with open(dmp_file) as dmp:
         
         except IndexError:
             print(f"No se ha encontrado el organismo con taxid {dmp}.")
-    # guardar el diccionario como json
+            error_ids_list.append(dmp)
+
+# guardar el diccionario como json
 
 with open("bact_dict.json", "w") as outfile:
     json.dump(bact_dict, outfile)
+
+
+if len(error_ids_list) == 1:
+    error_ids_list.append("Ninguno, todo satisfactorio")
+
+with open("IndexErrorTaxids.txt", "w") as outfile:
+    outfile.write(f"{error_ids_list [0]}\n")
+    for failed_taxid in error_ids_list[1:]:
+        print(f'-{failed_taxid}\n')

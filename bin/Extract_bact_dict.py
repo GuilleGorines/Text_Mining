16afeb9528ba0c6802ee37aa720a0208ddf81fca
GuillermoGorines <60110848+GuillermoGorines@users.noreply.github.com>
2021-02-27 19:47:44 +0100

    handled keyerror

--- bin/Extract_bact_dict.py
+++ bin/Extract_bact_dict.py
@@ -9,15 +9,18 @@ bact_dict = {}
 with open(dmp_file) as dmp:
     dmp = dmp.readlines()
     for organism in dmp:
-        search = Entrez.efetch(db='taxonomy',id=organism)
-        result = Entrez.read(search)
-        result = dict(result[0])
+        try:
+            search = Entrez.efetch(db='taxonomy',id=organism)
+            result = Entrez.read(search)
+            result = dict(result[0])
 
-        if result["LineageEx"][-1]["Rank"] == "genus":
-            bact_genera = result["ScientificName"][-1].lower()
-            bact_species = result["ScientificName"].lower()
-            bact_dict.setdefault(bact_genera,[]).append(bact_species)
-
+            if result["LineageEx"][-1]["Rank"] == "genus":
+                bact_genera = result["ScientificName"][-1].lower()
+                bact_species = result["ScientificName"].lower()
+                bact_dict.setdefault(bact_genera,[]).append(bact_species)
+        
+        except IndexError:
+            print(f"No se ha encontrado el organismo con taxid {dmp}.")
     # guardar el diccionario como json
 
 with open("bact_dict.json", "w") as outfile:

6fd74154c9bcdcd25679d1d70562c7d7fb6c89cb
GuilleGorines <guillermo.gorines.trabajo@gmail.com>
2021-03-10 18:46:34 +0100

    apparently, fstrings cant newline. Sad.

--- bin/Extract_bact_txt_raw.py
+++ bin/Extract_bact_txt_raw.py
@@ -5,7 +5,7 @@ import time
 dmp_file = sys.argv[1]
 Entrez.email="" # Necesario o error
 error_ids_list = ["IDs que no han podido incluirse en el diccionario:"]
-
+nl = 
 with open(dmp_file, "r+") as dmp, \
     open("bact_raw_dict.txt","w") as resultfile, \
     open("failed_taxids.dmp","w") as failedfile, \
@@ -19,16 +19,15 @@ with open(dmp_file, "r+") as dmp, \
             result = dict(result[0])
 
             if result["LineageEx"][-1]["Rank"] == "genus":
-                bact_genera = result["ScientificName"][-1].lower()
                 bact_species = result["ScientificName"].lower()
-                resultfile.write(f"{bact_genera} #### {bact_species}\n")
-                currenttaxids.write(f"{organism}\n")
+                resultfile.write("\n".join(bact_species))
+                currenttaxids.writelines("\n".join(organism))
                 dmp.remove(organism)
         except:
-            failedfile.write(f"{dmp}\n")
+            failedfile.write("\n".join(dmp))
         
         time.sleep(0.35)
 
 with open("IndexErrorTaxids.txt", "w") as outfile:
     for failed_taxid in error_ids_list:
-        outfile.write(f'{failed_taxid}\n')
+        outfile.write("\n".join(failed_taxid))

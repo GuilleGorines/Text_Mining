3f497d11695ac0f0894768cc96935e5e8b28901d
GuilleGorines <guillermo.gorines@gmail.com>
2022-10-27 13:58:20 +0200

    time format

--- wrappers/full_corpora_extraction.sh
+++ wrappers/full_corpora_extraction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-printf "$(date '+%Y%m%d%H%M%S'): ID extraction begins\n"
+printf "$(date '+%Y-%m-%d, %H:%M:%S') : ID extraction begins.\n"
 python3 ../bin/Extract_corpora_ids.py "septicemia or bacteremia or sepsis" "septic"
-printf "$(date '+%Y%m%d%H%M%S'): ID extraction completed.\n"
-printf "$(date '+%Y%m%d%H%M%S'): Corpora extraction begins.\n"
+printf "$(date '+%Y-%m-%d, %H:%M:%S') : ID extraction completed.\n"
+printf "$(date '+%Y-%m-%d, %H:%M:%S') : Corpora extraction begins.\n"
 python3 ../bin/Extract_corpora_from_ids.py "septic_ids.txt" "septic"
-printf "$(date '+%Y%m%d%H%M%S'): Corpora extraction completed"
+printf "$(date '+%Y-%m-%d, %H:%M:%S') : Corpora extraction completed."
